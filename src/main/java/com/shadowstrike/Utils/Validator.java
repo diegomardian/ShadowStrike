f18426a417ca332d05ec581462d914c4e213641e
jschlob <47754528+jschlob@users.noreply.github.com>
2021-02-01 19:42:46 -0700

    Add Intial Files

---
+++ src/main/java/com/shadowstrike/Utils/Validator.java
@@ -0,0 +1,15 @@
+package com.shadowstrike.Utils;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author root
+ */
+public abstract class Validator {
+    
+}
